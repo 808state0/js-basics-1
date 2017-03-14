0e0aefd8b1b4420b71e6ab944d52d0e21298681f
Michael <michaelanguay@outlook.com>
2017-03-13 16:15:14 -1000

    finished problem 2 and 3

--- basics.js
+++ basics.js
@@ -4,16 +4,24 @@ var myName = "Michael (Kaleo) Anguay";
  * `name`, assign it the same name as before,
  * as well as an `age` (number);
  */
+ var person = {
+  name: "Michael (Kaleo) Anguay",
+  age: 23
+ };
 
 /* Create a variable called `canDrive`,
  * if it should be true if your person object is at least 16 years old
  */
+ var canDrive = person.age >16;
 
 /* Create a function called `greet`,
  * it should take a 1 parameter, `name`
  * and it should print "Hello, my name is {name}"
  */
-
+function greet(name){
+  return  name;
+}
+console.log(greet();
 /* Create an array called `dataTypes` with atleast 1 of every data type;
  * (there are 6 different data types);
  */
